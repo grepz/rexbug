600efbc066da2d5ae9fc249b30cd43d919614b53
grepz <grepz@eternote.eterall>
2021-03-06 00:22:30 +0300

    Typo fix

--- lib/rexbug/printing/utils.ex
+++ lib/rexbug/printing/utils.ex
@@ -1,7 +1,7 @@
 defmodule Rexbug.Printing.Utils do
   @moduledoc false
 
-  @printing_opts [limit: :unlimited]
+  @printing_opts [limit: :infinity]
 
   def printing_inspect(term) do
     inspect(term, @printing_opts)
