571a0c42ab254494f4b9a87d09cb6dc1815af04d
grepz <grepz@eternote.eterall>
2021-03-06 00:15:47 +0300

    Fixing issue with printing and 1.11 elixir

--- lib/rexbug/printing/utils.ex
+++ lib/rexbug/printing/utils.ex
@@ -1,7 +1,7 @@
 defmodule Rexbug.Printing.Utils do
   @moduledoc false
 
-  @printing_opts [limit: -1]
+  @printing_opts [limit: :unlimited]
 
   def printing_inspect(term) do
     inspect(term, @printing_opts)
